b5d36d6d39a86aa6e226b77745ba9d8692d821cc
Munijee Singh <67405362+msingh111@users.noreply.github.com>
2020-07-19 16:35:29 +0530

    Add files via upload

---
+++ List_Tuple.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 07:32:52 2020
+
+@author: msingh31
+"""
+
+
+a = "    We are   learning PyThon  "
+print(a)
+
+a.islower()
+a.lower()
+a.isupper()
+a.upper()
+a.istitle()
+a.title()
+a.isalpha()
+a.isnumeric()
+a.isalnum()
+a.capitalize()
+a.count('e',1)
+a.endswith('n')
+a.find('are',1)
+a[3:6]
+a.strip()
+a.title()
+a.strip().replace('  ', ' ')
+
+list1 = ['Movie', 'Python', 3, 5.5]
+list1.append('ML')
+list1.insert(2,'DS')
+list1.pop()
+list1.remove(list1[2])
+list1.remove('DS')
+list1.clear()
+del list1
+list1.count(3)
+list1.index(3)
+
+
